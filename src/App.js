0c44ea4ba56fcfb00e8ac1386e8b6ac404614dd1
Alexandra Veres <Alexandra.Veres@cancer.org.uk>
2020-03-21 16:48:19 +0000

    Changed page layout.

--- src/App.js
+++ src/App.js
@@ -5,39 +5,29 @@ import "./App.css";
 function App() {
   const [movie, setMovies] = useState("no");
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/movies",  {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'charset':'UTF-8'
-       }
-
-    }).then(response =>
-      response.json().then(data => {
-        setMovies(data.movie);
-      })
-      ).catch(err => {
+    fetch("http://localhost:5000/probability", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        charset: "UTF-8"
+      }
+    })
+      .then(response =>
+        response.json().then(data => {
+          setMovies(data.probability);
+        })
+      )
+      .catch(err => {
         // Do something for an error here
         console.log("Error Reading data " + err);
-      })
- 
+      });
   });
   return (
     <div className="App">
-      <div>{movie}</div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{fontSize: 50}}>
+          <code>Probability of 1: {movie}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
