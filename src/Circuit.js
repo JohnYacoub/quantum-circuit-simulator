0c1c354b045729139191fc394410eb59a3bf69b7
XandraV <veresalexandra95@gmail.com>
2020-03-27 12:21:58 +0000

    Added fade to svg circuit.

--- src/Circuit.js
+++ src/Circuit.js
@@ -8,18 +8,20 @@ function Circuit(props) {
         xmlns="http://www.w3.org/2000/svg"
         //viewBox="0 0 841.9 595.3"
       >
-        <line
-          x1={0}
-          x2={props.gateList.length * 2 * 40} //listLength*2*40
-          y1={20}
-          y2={20}
-          fill="none"
-          stroke="black"
-          style={{ strokeWidth: 2, strokeLinecap: "round" }}
-        ></line>
+       
         {props.gateList.map((gate, idx) => (
-          <g key={gate}>
+         
+          <g key={gate}> <line
+          className="testtest"
+            x1={80 * idx + 40}
+            x2={80 * idx + 40 +40}
+            y1={20}
+            y2={20}
+            fill="none"
+            style={{ stroke:"black", strokeWidth: 2, strokeLinecap: "round" }}
+          ></line>
             <rect
+            className="test"
               x={80 * idx} //80*(elemNum-1)
               y={0}
               width={40}
@@ -27,6 +29,7 @@ function Circuit(props) {
               style={{ fill: "rgb(253, 184, 19)" }}
             ></rect>
             <text
+              className="test"
               dominantBaseline="alphabetical"
               x={20 + 80 * idx} //20+80*elemNum
               y={26}
